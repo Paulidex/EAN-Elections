b25ead69c5b5f3567f4a02af399d71ed12dd8244
Juan Felipe Rojas <jrojash10040@universidadean.edu.co>
2025-05-24 02:13:22 -0500

    This is a test commit

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_application_1/views/profile.dart';
 import 'firebase_options.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+
 
 
 
